8ae2e14317f56f9bf88a0637d5203f7835437963
Ayon <mirelahi33@gmail.com>
2022-03-31 20:27:56 -0400

    Learning Useeffect

--- react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [size, serSize] = useState(window.innerWidth);
+  const handle = () => {
+    serSize(window.innerWidth);
+  };
+  useEffect(() => {
+    console.log("heyy");
+    window.addEventListener("resize", handle);
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("resize", handle);
+    };
+  }, []);
+  return (
+    <>
+      <h2>window {size}</h2>
+    </>
+  );
 };
 
 export default UseEffectBasics;
