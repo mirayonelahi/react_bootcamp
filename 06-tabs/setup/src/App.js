6408d4e7f52e6241f85013959a43a4afb7f1d96b
Ayon <mirelahi33@gmail.com>
2022-04-05 18:05:25 -0400

    working on tab project

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const dataApi = await response.json();
+      setData(dataApi);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [data]);
+
+  if (loading) {
+    return (
+      <section className="title">
+        <h1>loading</h1>
+      </section>
+    );
+  }
+
+  return (
+    <main>
+      <section></section>
+    </main>
+  );
 }
 
-export default App
+export default App;
