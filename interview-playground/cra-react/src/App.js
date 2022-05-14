40bae47269f61bbeda2ed33fd427a2d43e26b1f6
Ayon <mirelahi33@gmail.com>
2022-05-14 01:25:08 -0400

    practising random react for interview

---
+++ interview-playground/cra-react/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold p-8">Hello world!</h1>
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
